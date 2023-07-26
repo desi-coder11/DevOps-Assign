0252b89f98ebb2a5ae58ae97cd221af8c3e077aa
desi-coder11 <65103107+desi-coder11@users.noreply.github.com>
2023-07-26 11:10:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
